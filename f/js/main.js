aa2697124ad7f0dc5cdd05dbe49f35d4a8ec713d
r-black <romaprudnickov@yandex.ru>
2018-05-13 03:59:37 +0300

    adaptive

--- f/js/main.js
+++ f/js/main.js
@@ -1,1 +1,15 @@
-//Here will be code javascript
+
+function selectPack(){
+    this.classList.toggle('selected');
+}
+
+var packs = document.getElementsByClassName('page-products__item-bg');
+for(var i=0; i<packs.length;i++){
+    if(!packs[i].classList.contains('disabled')) {
+        packs[i].addEventListener('click', selectPack);
+    }
+}
+
+document.querySelector('.page-products__description-after-item--link').addEventListener('click', function() {
+		document.querySelector('.page-products__item-bg').classList.toggle('selected');
+});
